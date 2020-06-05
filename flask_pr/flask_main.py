1e6c09d744ae8b89c4f0019b1fbac2c73093f601
mweapon <marco.serranomo@udlap.mx>
2020-06-05 18:04:18 -0500

    commit_2: new webpages

--- flask_pr/flask_main.py
+++ flask_pr/flask_main.py
@@ -1,30 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='pages')
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page!<h1>"
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return "<h1>About Page!<h1>"
+    return render_template('about.html')
 
 @app.route('/schools')
 def schools():
-    return "<h1>Schools Page!<h1>"
+    return render_template('schools.html')
 
-@app.route('/services')
-def services():
-    return "<h1>Services Page!<h1>"
+@app.route('/therapies')
+def therapies():
+    return render_template('therapies.html')
 
 @app.route('/contact')
 def contact():
-    return "<h1>Contact Page!<h1>"
+    return render_template('contact.html')
 
 @app.route('/business')
 def business():
-    return "<h1>business Page!<h1>"
+    return render_template('business.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
